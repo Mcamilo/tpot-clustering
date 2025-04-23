e941d4f17c46eb70bd99a551e617c42dddc11d59
Mcamilo <matheuscmilo@gmail.com>
2025-04-23 18:42:39 +0200

    first commit

---
+++ tpot/tpot.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""This file is part of the TPOT library.
+
+TPOT was primarily developed at the University of Pennsylvania by:
+    - Randal S. Olson (rso@randalolson.com)
+    - Weixuan Fu (weixuanf@upenn.edu)
+    - Daniel Angell (dpa34@drexel.edu)
+    - and many more generous open source contributors
+
+TPOT is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+TPOT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with TPOT. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+from .base import TPOTBase
+from .config.clustering import clustering_config_dict
+
+class TPOTClustering(TPOTBase):
+    """TPOT estimator for clustering problems."""
+    scoring_function = 'silhouette_score'  # Clustering scoring
+    default_config_dict = clustering_config_dict
+    clustering = True
+    
+    def _init_pretest(self, features, target=None):
+        self.pretest_X = features
+        self.pretest_y = target
---
+++ tpot_clustering_silhouette_score.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+
+# NOTE: Make sure that the csv file with the doesn't contain targets
+training_features = pd.read_csv('PATH/TO/DATA/FILE', dtype=np.float64)
+
+# Average CV score on the training set was: 0.7072801240568757
+
+# Standardize the data
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(training_features)
+
+# Perform PCA
+pca = PCA(n_components=2)
+pca_data = pca.fit_transform(scaled_data)
+        
+exported_pipeline = KMeans(init="random", n_clusters=2)
+clusters = exported_pipeline.fit_predict(pca_data)
+
+# Plot PCA
+plt.figure(figsize=(10, 7))
+plt.scatter(pca_data[:, 0], pca_data[:, 1], c=clusters, cmap='viridis', marker='o', edgecolor='k', s=100)
+if hasattr(exported_pipeline,"cluster_centers_"):
+    centroids = exported_pipeline.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='X', s=200, alpha=0.75)
+plt.title('PCA of the Dataset with the exported clustering pipeline')
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.grid(True)
+
+# Save the PCA plot
+plt.savefig('pca_plot.png')
+
+print("PCA plot saved as 'pca_plot.png'.")
